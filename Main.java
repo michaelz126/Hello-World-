57ab31591e546ddc6d3a72cbcfe41b4e1b88e59a
michaelz126 <1zhang.michael@gmail.com>
2024-09-13 09:24:43 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        String word = "code";
+        System.out.println(word);
         System.out.print("Hello World!");
     }
 }
